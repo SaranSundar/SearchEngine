2cabdc6379b690ed7ec8cf758169c74320147cf8
Saran Sundararajan <saransszg@gmail.com>
2019-11-26 16:46:15 -0600

    Can index

--- src/components/Indexes/Indexes.js
+++ src/components/Indexes/Indexes.js
@@ -2,7 +2,6 @@ import React, {Component} from 'react';
 import './Indexes.css';
 import TextField from "@material-ui/core/TextField";
 import Container from "@material-ui/core/Container";
-import TextareaAutosize from '@material-ui/core/TextareaAutosize';
 import Grid from "@material-ui/core/Grid";
 import {Typography} from "@material-ui/core";
 import Fab from '@material-ui/core/Fab';
@@ -10,6 +9,7 @@ import AddIcon from '@material-ui/icons/Add';
 import CircularShift from "../../backend/CircularShift";
 import Alphabetizer from "../../backend/Alphabetizer";
 import Denoiser from "../../backend/Denoiser";
+import CircularProgress from "@material-ui/core/CircularProgress";
 
 class Indexes extends Component {
     constructor(props) {
@@ -19,11 +19,28 @@ class Indexes extends Component {
             circularShifts: "",
             alphaShifts: "",
             denoiseShifts: "",
+            showLoader: false,
         };
         this.circularShift = new CircularShift();
         this.alphaShift = new Alphabetizer();
         this.denoiseShift = new Denoiser();
     }
+
+    createNewIndexes = async () => {
+        this.setState({showLoader: true});
+        const response = await fetch('http://localhost:8000/server_bp/index', {
+            crossDomain: true,
+            method: 'POST',
+            headers: {'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*',},
+            body: JSON.stringify({
+                url: this.state.search
+            }),
+        });
+        let body = await response.json();
+        console.log(body);
+        alert("Message: " + body['message']);
+        this.setState({showLoader: false});
+    };
 
     startShifts = () => {
         this.setState({alphaShifts: "", denoiseShifts: ""}, () => {
@@ -64,7 +81,7 @@ class Indexes extends Component {
                 <Container className="Indexes-Container">
                     <TextField
                         id="search"
-                        placeholder="Search"
+                        placeholder="Enter URL"
                         value={this.state.search}
                         className="Indexes-SearchField"
                         onChange={this.handleChange('search')}
@@ -72,56 +89,66 @@ class Indexes extends Component {
                         variant="outlined"
                         onKeyPress={(ev) => {
                             if (ev.key === 'Enter') {
-                                this.startShifts();
+                                this.createNewIndexes();
                                 ev.preventDefault();
                             }
                         }}
                     />
-                    <Fab onClick={this.startShifts} style={{marginTop: "10px"}} color="primary" aria-label="add">
+                    <Fab onClick={this.createNewIndexes} style={{marginTop: "10px"}} color="primary" aria-label="add">
                         <AddIcon/>
                     </Fab>
                 </Container>
                 <Grid container className="Indexes-Grid">
-                    <Grid item>
-                        <Typography className="Indexes-Grid-Title" variant="h5" component="h4">
-                            Circular Shift
-                        </Typography>
-                        <TextareaAutosize
-                            disabled={true}
-                            rows={30}
-                            className="Indexes-TextArea"
-                            aria-label="circular_shift"
-                            placeholder="Lines to Circular Shift"
-                            value={this.state.circularShifts}
-                        />
-                    </Grid>
-                    <Grid item>
-                        <Typography className="Indexes-Grid-Title" variant="h5" component="h4">
-                            Alphabetizer
-                        </Typography>
-                        <TextareaAutosize
-                            disabled={true}
-                            rows={30}
-                            className="Indexes-TextArea"
-                            aria-label="alphabetizer"
-                            placeholder="Lines to Alphabetize"
-                            value={this.state.alphaShifts}
-                        />
-                    </Grid>
-                    <Grid item>
-                        <Typography className="Indexes-Grid-Title" variant="h5" component="h4">
-                            Noise Eliminator
-                        </Typography>
-                        <TextareaAutosize
-                            disabled={true}
-                            rows={30}
-                            className="Indexes-TextArea"
-                            aria-label="denoiser"
-                            placeholder="Lines to Denoise"
-                            value={this.state.denoiseShifts}
-                        />
-                    </Grid>
+                    {this.state.showLoader &&
+                    <Typography variant="h5">
+                        Indexing...
+                    </Typography>
+                    }
+                    {/*<Grid item>*/}
+                    {/*    <Typography className="Indexes-Grid-Title" variant="h5" component="h4">*/}
+                    {/*        Circular Shift*/}
+                    {/*    </Typography>*/}
+                    {/*    <TextareaAutosize*/}
+                    {/*        disabled={true}*/}
+                    {/*        rows={30}*/}
+                    {/*        className="Indexes-TextArea"*/}
+                    {/*        aria-label="circular_shift"*/}
+                    {/*        placeholder="Lines to Circular Shift"*/}
+                    {/*        value={this.state.circularShifts}*/}
+                    {/*    />*/}
+                    {/*</Grid>*/}
+                    {/*<Grid item>*/}
+                    {/*    <Typography className="Indexes-Grid-Title" variant="h5" component="h4">*/}
+                    {/*        Alphabetizer*/}
+                    {/*    </Typography>*/}
+                    {/*    <TextareaAutosize*/}
+                    {/*        disabled={true}*/}
+                    {/*        rows={30}*/}
+                    {/*        className="Indexes-TextArea"*/}
+                    {/*        aria-label="alphabetizer"*/}
+                    {/*        placeholder="Lines to Alphabetize"*/}
+                    {/*        value={this.state.alphaShifts}*/}
+                    {/*    />*/}
+                    {/*</Grid>*/}
+                    {/*<Grid item>*/}
+                    {/*    <Typography className="Indexes-Grid-Title" variant="h5" component="h4">*/}
+                    {/*        Noise Eliminator*/}
+                    {/*    </Typography>*/}
+                    {/*    <TextareaAutosize*/}
+                    {/*        disabled={true}*/}
+                    {/*        rows={30}*/}
+                    {/*        className="Indexes-TextArea"*/}
+                    {/*        aria-label="denoiser"*/}
+                    {/*        placeholder="Lines to Denoise"*/}
+                    {/*        value={this.state.denoiseShifts}*/}
+                    {/*    />*/}
+                    {/*</Grid>*/}
                 </Grid>
+                {this.state.showLoader &&
+                <Grid container className="Indexes-Grid" style={{marginTop: "10px"}}>
+                    <CircularProgress/>
+                </Grid>
+                }
             </div>
         );
     }
