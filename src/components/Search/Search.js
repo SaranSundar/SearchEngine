56cb4ae54575398da671377f11070bd5cb189a00
Saran Sundararajan <saransszg@gmail.com>
2019-11-26 14:09:43 -0600

    Shows UI

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -2,26 +2,24 @@ import React, {Component} from 'react';
 import '../Indexes/Indexes.css';
 import TextField from "@material-ui/core/TextField";
 import Container from "@material-ui/core/Container";
-import {Typography} from "@material-ui/core";
+import {Card, CardContent, Typography} from "@material-ui/core";
 import Fab from '@material-ui/core/Fab';
 import AddIcon from '@material-ui/icons/Add';
-import CircularShift from "../../backend/CircularShift";
-import Alphabetizer from "../../backend/Alphabetizer";
-import Denoiser from "../../backend/Denoiser";
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import FavoriteBorder from '@material-ui/icons/FavoriteBorder';
+import Favorite from '@material-ui/icons/Favorite';
 
 class Search extends Component {
     constructor(props) {
         super(props);
         this.state = {
             search: "",
-            circularShifts: "",
-            alphaShifts: "",
-            denoiseShifts: "",
-            case_sensitive: false
+            case_sensitive: false,
+            urls: null,
+            descriptions: [],
+            titles: [],
         };
-        this.circularShift = new CircularShift();
-        this.alphaShift = new Alphabetizer();
-        this.denoiseShift = new Denoiser();
     }
 
     handleChange = name => event => {
@@ -35,7 +33,41 @@ class Search extends Component {
             headers: {'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*',},
             body: JSON.stringify({query: this.state.search, case_sensitive: this.state.case_sensitive}),
         });
-        console.log(await response.json())
+        let body = await response.json();
+        console.log(body);
+        this.setState({urls: body['urls'], descriptions: body['descriptions'], titles: body['titles']});
+    };
+
+    displayResults = () => {
+        if (this.state.urls === null) {
+            return;
+        }
+        if (this.state.urls.length === 0) {
+            return (
+                <Card style={{margin: "5px"}}>
+                    <CardContent>
+                        No Results
+                    </CardContent>
+                </Card>);
+        }
+        return this.state.urls.map(((url, index) =>
+                <Card style={{margin: "5px"}}>
+                    <CardContent>
+                        <a href={this.state.urls[index]} style={{cursor: "pointer", textDecoration: "none"}}
+                           target="_blank">
+                            <Typography gutterBottom variant="h5" style={{color: "blue"}}>
+                                {this.state.titles[index]}
+                            </Typography>
+                            <Typography gutterBottom variant="h5" style={{color: "green"}}>
+                                {this.state.urls[index]}
+                            </Typography>
+                        </a>
+                        <Typography gutterBottom variant="h5">
+                            {this.state.descriptions[index]}
+                        </Typography>
+                    </CardContent>
+                </Card>
+        ))
     };
 
     render() {
@@ -45,6 +77,10 @@ class Search extends Component {
                     Zephyr
                 </Typography>
                 <Container className="Indexes-Container">
+                    <FormControlLabel
+                        control={<Checkbox icon={<FavoriteBorder />} checkedIcon={<Favorite />} value="checkedH" />}
+                        label="Case Sensitive"
+                    />
                     <TextField
                         id="search"
                         placeholder="Search"
@@ -63,6 +99,9 @@ class Search extends Component {
                     <Fab onClick={this.searchIndexes} style={{marginTop: "10px"}} color="primary" aria-label="add">
                         <AddIcon/>
                     </Fab>
+                    <Container>
+                        {this.displayResults()}
+                    </Container>
                 </Container>
             </div>
         );
